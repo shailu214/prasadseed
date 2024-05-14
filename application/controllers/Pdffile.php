7eb9f517dc3df2f82840c54f1c2b4470b58a8a91
Shailendra <chaurasiashailendra93@gmail.com>
2024-05-14 23:34:05 +0530

    pdffile

---
+++ application/controllers/Pdffile.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pdffile extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		if( $this->cmsval->validateApp() ) {
+			redirect('login');
+		}
+		if(empty($_SESSION[ADMIN])) { redirect(''); }
+		//if($_SESSION[ADMIN]['role'] != 1 && !in_array(1,$_SESSION[ADMIN]['prms'])) { redirect('dashboard'); }
+		$this->pageParam->nav = 'pdffile';
+		$this->pageParam->role = $_SESSION[ADMIN]['role'];
+	}
+
+    public function index( $pg=null ) {
+
+		
+		$this->load->view('admin/challan/pdf',$data);
+		$this->load->view('admin/footer');
+	}
+
+    public function view($id) {
+		$this->db->where('id', $id);
+		$obj = $this->db->get("fare_gadi_bada")->row();
+		
+		$this->db->where('id', $obj->farmer_id);
+		$farmer = $this->db->get("farmer")->row();
+		if($farmer) {
+			$obj->farmer_id = $farmer->name;
+		}
+
+		$data = ['obj' => $obj];
+		$this->load->view('admin/challan/info',$data);
+		$this->load->view('admin/footer');
+	}
+
+
+
+
+}
