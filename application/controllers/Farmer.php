7773434abc48b6df39c63b528c82b75585494ccd
vikas <amazinvikas.com>
2024-05-15 18:11:16 +0530

    show lots

--- application/controllers/Farmer.php
+++ application/controllers/Farmer.php
@@ -126,10 +126,14 @@ class Farmer extends CI_Controller {
 		$data['bardana'] = $this->bardanaList($id, $search);
 		$data['amount'] = $this->amountList($id, $search);
 		$data['fare'] = $this->fareList($id, $search);
-
+		
+		$this->db->where('farmer_id', $id);
+		$obj = $this->db->get("sell")->result();
+		$data['lots'] = $obj;
+		
 		$this->db->where('farmer_id', $id);
 		$data['resultsell'] = $this->db->get("sell")->result_array();
-		//echo '<pre>'; print_r($data['resultsell']); die;
+		//echo '<pre>'; print_r($data['lots']); die;
 		$this->load->view('admin/head');
 		$this->load->view('admin/header');
 		$this->load->view('admin/farmer/info',$data);
