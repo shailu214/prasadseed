9c73fb17f0c427b4ddc9f387020875fc7d17b239
vikas <amazinvikas.com>
2024-05-18 12:48:09 +0530

    pdf hindi content

--- application/views/admin/challan/pdf.php
+++ application/views/admin/challan/pdf.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <style>
 
 .header{
@@ -303,3 +312,6 @@ $("body").on("click", "#downloadPDF", function () {
   });
 });
 </script>
+
+</body>
+</html>
